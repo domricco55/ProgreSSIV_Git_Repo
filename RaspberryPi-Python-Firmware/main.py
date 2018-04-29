357acaeac95c98234ae043b8eec46e52cb551c61
Dom <domricco55@gmail.com>
2018-04-29 07:25:34 -0700

    Charlie stuff, added a for loop that sends 10 messages.

--- RaspberryPi-Python-Firmware/main.py
+++ RaspberryPi-Python-Firmware/main.py
@@ -8,24 +8,26 @@ Created on Fri Apr 27 22:51:05 2018
 
 import wiringpi
 import struct
+import time
 
 wiringpi.wiringPiSetupGpio()
 SPIChannel = 0
-SPIFrequency = 122000
+SPIFrequency = 122000/2
 wiringpi.wiringPiSPISetup(SPIChannel,SPIFrequency)
 
 read_bit = 0b10000000 #read bit
 write_bit = 0b0000000 #write bit
-address = 0
+address = 0 #send an address in the first byte
 data = 0
-#buf = bytes(127)
-buf = struct.pack('B', address | read_bit, data)
-#buf ='\x00'
 
-print(buf.decode('utf8'))
 
-retlen, retdata = wiringpi.wiringPiSPIDataRW(SPIChannel,buf)
-print(buf)
+for n in range(10):
+    buf = struct.pack('BBBBB', address | read_bit, data,1,1,1)
 
-print ('retlen ', retlen)
-print ('retdata ',retdata)
+    retlen, retdata = wiringpi.wiringPiSPIDataRW(SPIChannel,buf)
+    #print(buf)
+    
+    print (struct.unpack('BBBBB',retdata))
+    #print ('retlen ', retlen)
+    #print ('retdata ',retdata)
+    time.sleep(1)
