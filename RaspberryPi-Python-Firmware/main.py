c3998d1f97d5ec32b03be6bd7d4036b37c10d379
Dom <domricco55@gmail.com>
2018-04-29 04:47:31 -0700

    Worked out the sending of an integer using struct.pack('B', integer) and also encoded some read and write bit masks for readability. This code currently sends an spi message correctly using the wiringpi functions in a manner not shown in the documentation.

--- RaspberryPi-Python-Firmware/main.py
+++ RaspberryPi-Python-Firmware/main.py
@@ -6,3 +6,26 @@ Created on Fri Apr 27 22:51:05 2018
 @author: pi
 """
 
+import wiringpi
+import struct
+
+wiringpi.wiringPiSetupGpio()
+SPIChannel = 0
+SPIFrequency = 122000
+wiringpi.wiringPiSPISetup(SPIChannel,SPIFrequency)
+
+read_bit = 0b10000000 #read bit
+write_bit = 0b0000000 #write bit
+address = 0
+data = 0
+#buf = bytes(127)
+buf = struct.pack('B', address | read_bit, data)
+#buf ='\x00'
+
+print(buf.decode('utf8'))
+
+retlen, retdata = wiringpi.wiringPiSPIDataRW(SPIChannel,buf)
+print(buf)
+
+print ('retlen ', retlen)
+print ('retdata ',retdata)
